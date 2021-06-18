548cd8005d675cb8dbdd9385beb3b8c9559c6b7a
togcursos <togcursos@gmail.com>
2021-06-18 16:59:46 -0500

    Eliminados

--- js/init.js
+++
@@ -1,9 +0,0 @@
-firebase.initializeApp({
-    apiKey: "AIzaSyBay7Xwaqy1cta-lihsSRmIRhM1BSiTmIw",
-    authDomain: "togcursos04.firebaseapp.com",
-    projectId: "togcursos04",
-    storageBucket: "togcursos04.appspot.com",
-    messagingSenderId: "176719754779",
-    appId: "1:176719754779:web:0866404d294ef7bdfa6abe",
-    measurementId: "G-CEWP5SHTHT"
-});

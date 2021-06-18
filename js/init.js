79aed561ce903436e68ebd5979a9670cdb0c5818
togcursos <togcursos@gmail.com>
2021-06-18 09:15:38 -0500

    Actualizo

--- js/init.js
+++ js/init.js
@@ -1,9 +1,9 @@
 firebase.initializeApp({
-    apiKey: "AIzaSyCapbjMgK9468ICMWWnyrVvTbteXFOx_8E",
-    authDomain: "propweb39.firebaseapp.com",
-    projectId: "propweb39",
-    storageBucket: "propweb39.appspot.com",
-    messagingSenderId: "86992270502",
-    appId: "1:86992270502:web:7c027b65446d5b5102b067",
-    measurementId: "G-RFGPZ8Y89S"
+    apiKey: "AIzaSyArRJTU1vCECFwctPwcXWqf0Yl2yrSUXEs",
+    authDomain: "cursostogc.firebaseapp.com",
+    projectId: "cursostogc",
+    storageBucket: "cursostogc.appspot.com",
+    messagingSenderId: "640671366700",
+    appId: "1:640671366700:web:d6e250203259f707379b89",
+    measurementId: "G-9GV935VLSR"
 });
